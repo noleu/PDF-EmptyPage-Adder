8d7d0f0ab6204f0a0192d9ed230b593678cfa339
Tiels Thiele <noleu66@posteo.net>
2023-09-03 18:31:44 +0200

    switched to .Net7 project

---
+++ PageAdder/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace PageAdder;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }   
+}
+ 
